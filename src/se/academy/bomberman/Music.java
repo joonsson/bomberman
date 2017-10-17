65b81c8333475532f76a4cd51d99a0e371ec4edb
joonsson <joonsson@gmail.com>
2017-10-17 13:08:59 +0200

    Music done

--- src/se/academy/bomberman/Music.java
+++ src/se/academy/bomberman/Music.java
@@ -2,6 +2,9 @@ package se.academy.bomberman;
 
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.util.Duration;
+
+import java.io.File;
 
 public class Music extends Thread {
     MediaPlayer mediaPlayer;
@@ -12,12 +15,21 @@ public class Music extends Thread {
         this(file, false);
     }
     public Music(String file, boolean loop) {
-        media = new Media(file);
+        media = new Media(new File(file).toURI().toString());
         mediaPlayer = new MediaPlayer(media);
         this.loop = loop;
     }
     @Override
     public void run() {
-
+        mediaPlayer.play();
+        if (loop) {
+            mediaPlayer.setOnEndOfMedia(new Runnable() {
+                @Override
+                public void run() {
+                    mediaPlayer.seek(Duration.ZERO);
+                    mediaPlayer.play();
+                }
+            });
+        }
     }
 }
