0ab5374e125db1df222e811f942af3438e295e83
Patrik Fridström <frizze18@gmail.com>
2017-10-18 14:18:01 +0200

    started with PowerUps

---
+++ src/se/academy/bomberman/PowerUp.java
@@ -0,0 +1,24 @@
+package se.academy.bomberman;
+
+public class PowerUp {
+
+    private double speed;
+    private boolean inmortal;
+    private boolean biggerBombs;
+
+    public PowerUp (PlayerHoes speed ){
+        this.speed = 1;
+
+    }
+
+    public double speed (){
+        this.speed =1.5;
+
+        return speed;
+    }
+
+    public void bigger(){
+
+
+    }
+}
