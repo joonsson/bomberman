2a29ffc0f0a52d9911ac42f368fe5fb5d4c12b85
joonsson <joonsson@gmail.com>
2017-10-18 16:45:38 +0200

    Added menu

--- src/se/academy/bomberman/Bomberman.java
+++ src/se/academy/bomberman/Bomberman.java
@@ -10,6 +10,9 @@ import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import javafx.embed.swing.JFXPanel;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -21,7 +24,9 @@ public class Bomberman {
     static protected final int WEST = 2;
     static protected final int EAST = 3;
     private static final int COLUMNS = 29;
+    private static final int SCREENWIDTH = 129;
     private static final int ROWS = 20;
+    private static final int SCREENHEIGHT = 49;
     private static final int BJSTARTX = 1;
     private static final int BHSTARTX = 25;
     private static final int BJSTARTY = 1;
@@ -32,33 +37,112 @@ public class Bomberman {
     public static void main(String[] args) throws IOException, InterruptedException {
         JFXPanel jfx = new JFXPanel();
 
-        Map map = new Map(COLUMNS, ROWS);
+        Map map = new Map(COLUMNS, ROWS); //TODO change where we draw the map and start pos
 
 
-        TerminalSize newSize = new TerminalSize(map.getColumns(), map.getRows());
+        TerminalSize newSize = new TerminalSize(SCREENWIDTH,SCREENHEIGHT);
         Screen screen = new DefaultTerminalFactory().setInitialTerminalSize(newSize).createScreen();
         screen.startScreen();
         Music brinstar = new Music("src/Sounds/Brinstar.mp3");
         //brinstar.start();
         screen.setCursorPosition(null);
+        boolean menu = true;
+        boolean start = true;
+        TextGraphics tg = screen.newTextGraphics();
+        while(menu) {
+            screen.clear();
+            BufferedReader reader = new BufferedReader(new FileReader(new File("src/Text/title.txt")));
+            String s = reader.readLine();
+            int n = 0;
+            while (s != null) {
+                tg.putString(15, n++, s);
+                s = reader.readLine();
+            }
+            reader = new BufferedReader(new FileReader(new File("src/Text/bomberman.txt")));
+            s = reader.readLine();
+            n--;
+            while (s != null) {
+                tg.putString(45, n++, s);
+                s = reader.readLine();
+            }
+            reader = new BufferedReader(new FileReader(new File("src/Text/start.txt")));
+            s = reader.readLine();
+            int startX = 20;
+            int startY = n;
+            while (s != null) {
+                tg.putString(30, n++, s);
+                s = reader.readLine();
+            }
+            reader = new BufferedReader(new FileReader(new File("src/Text/exit.txt")));
+            s = reader.readLine();
+            int exitX = 35;
+            int exitY = n;
+            while (s != null) {
+                tg.putString(45, n++, s);
+                s = reader.readLine();
+            }
+            reader = new BufferedReader(new FileReader(new File("src/Text/star.txt")));
+            if (start) {
+                s = reader.readLine();
+                n = startY;
+                while (s != null) {
+                    tg.putString(startX, n++, s);
+                    s = reader.readLine();
+                }
+            } else {
+                s = reader.readLine();
+                n = exitY;
+                while (s != null) {
+                    tg.putString(exitX, n++, s);
+                    s = reader.readLine();
+                }
+            }
+            screen.refresh();
+            KeyStroke key = null;
+            while (key == null) {
+                key = screen.pollInput();
+                if (key != null) {
+                    switch (key.getKeyType()) {
+                        case Enter:
+                            if (start) {
+                                menu = false;
+                                inGame = true;
+                                break;
+                            } else {
+                                screen.close();
+                                System.exit(0);
+                            }
+                        case ArrowUp:
+                            if (!start) start = true;
+                            break;
+                        case ArrowDown:
+                            if (start) start = false;
+                            break;
+                        default:
+                            key = null;
+                            break;
+                    }
+                }
+            }
+        }
         do {
             screen.clear();
-        draw(map.getCells(), screen);
+            draw(map.getCells(), screen);
 
-        Player player1 = new Player(BJSTARTX, BJSTARTY, 'J', new TextColor.RGB(180, 10, 140),
-                new TextColor.RGB(100, 4, 80), screen, new TextColor.RGB(255, 0, 0),
-                map.getCells()[BJSTARTX][BJSTARTY].color, new TextColor.RGB(180, 0, 0), map.getCells());
+            Player player1 = new Player(BJSTARTX, BJSTARTY, 'J', new TextColor.RGB(180, 10, 140),
+                    new TextColor.RGB(100, 4, 80), screen, new TextColor.RGB(255, 0, 0),
+                    map.getCells()[BJSTARTX][BJSTARTY].color, new TextColor.RGB(180, 0, 0), map.getCells());
 
-        Player player2 = new Player(BHSTARTX, BHSTARTY, 'H', new TextColor.RGB(0, 100, 200),
-                new TextColor.RGB(0, 40, 160), screen, new TextColor.RGB(255, 0, 0),
-                map.getCells()[BHSTARTX][BHSTARTY].color, new TextColor.RGB(180, 0, 0), map.getCells());
-        player2.setEnemy(player1);
-        player1.setEnemy(player2);
+            Player player2 = new Player(BHSTARTX, BHSTARTY, 'H', new TextColor.RGB(0, 100, 200),
+                    new TextColor.RGB(0, 40, 160), screen, new TextColor.RGB(255, 0, 0),
+                    map.getCells()[BHSTARTX][BHSTARTY].color, new TextColor.RGB(180, 0, 0), map.getCells());
+            player2.setEnemy(player1);
+            player1.setEnemy(player2);
 
-        List<Player> players = new ArrayList<>();
+            List<Player> players = new ArrayList<>();
 
-        players.add(player1);
-        players.add(player2);
+            players.add(player1);
+            players.add(player2);
 
             do {
                 long delay = System.currentTimeMillis();
@@ -85,17 +169,45 @@ public class Bomberman {
             Music gameOver = new Music("src/Sounds/smb_gameover.wav");
             Thread.sleep(3000);
             screen.clear();
-            TextGraphics tg = screen.newTextGraphics();
+            tg = screen.newTextGraphics();
             if (player1.isLiving()) {
-                tg.putString(6, 8, "Player1 wins!");
-                tg.putString(6, 12, "Player2 sucks!");
+                BufferedReader reader = new BufferedReader(new FileReader(new File("src/Text/p1win.txt")));
+                String s = reader.readLine();
+                int n = 10;
+                while (s != null) {
+                    tg.putString(2, n++, s);
+                    s = reader.readLine();
+                }
             } else {
-                tg.putString(6, 8, "Player2 wins!");
-                tg.putString(6, 12, "Player1 sucks!");
+                BufferedReader reader = new BufferedReader(new FileReader(new File("src/Text/p2win.txt")));
+                String s = reader.readLine();
+                int n = 10;
+                while (s != null) {
+                    tg.putString(2, n++, s);
+                    s = reader.readLine();
+                }
             }
-            
-        } while(true);
-
+            tg.putString(50, 28, "Press enter to play again.");
+            tg.putString(50, 30, "Press escape to exit.");
+            screen.refresh();
+            KeyStroke key;
+            menu = true;
+            while (menu) {
+                key = screen.pollInput();
+                if (key != null) {
+                    switch (key.getKeyType()) {
+                        case Enter:
+                            menu = false;
+                            inGame = true;
+                            break;
+                        case Escape:
+                            screen.close();
+                            System.exit(0);
+                            break;
+                    }
+                }
+            }
+        }while (true) ;
     }
 
     private static void draw(MapCell[][] mapCells, Screen screen) {
